8a14c064b33e883d04960b5a04ea178d3a45e3a9
xxDEE02xx <romnel.bunoan@viki.com>
2022-11-17 17:51:11 +0800

    Header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,11 +1,37 @@
 import React, { FC } from 'react';
 
 import './Header.css';
+import vikiLogo from '../../assets/viki-logo.svg';
+import communityLogo from '../../assets/community-logo-small-en.svg';
 
 const Header: FC = () => {
   return (
     <nav>
-      Header here
+      <div className="container">
+        <div className="logo">
+          <a href='#'>
+            <img src={vikiLogo} alt='Rakuten Viki' />
+          </a>
+          <ul>
+            <li />
+            <li />
+          </ul>
+          <a href='#'>
+            <img src={communityLogo} alt="community-logo" />
+          </a>
+        </div>
+        <div className="navigation">
+          <ul>
+            <li><a href='#'>Perks</a></li>
+            <li><a href='#'>Community Resources</a></li>
+            <li><a href='#'>Discussions</a></li>
+          </ul>
+        </div>
+        <div className="user-access">
+          <a href='#'>Login</a>
+          <button>Start your journey!</button>
+        </div>
+      </div>
     </nav>
   )
 }
